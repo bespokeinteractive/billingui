eed1dac38b2155709e272381b182b298100aa77c
ngarivictor <ngarivictor43@gmail.com>
2016-01-20 09:46:43 +0300

    get logic first page

---
+++ omod/src/main/java/org/openmrs/module/billingui/page/controller/BillingQueuePageController.java
@@ -0,0 +1,20 @@
+package org.openmrs.module.billingui.page.controller;
+
+import org.openmrs.ui.framework.page.PageModel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ngarivictor on 1/20/2016.
+ */
+public class BillingQueuePageController {
+    public void get(PageModel model){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String dateStr = sdf.format(new Date());
+        model.addAttribute("currentDate", dateStr);
+        
+
+    }
+
+}
