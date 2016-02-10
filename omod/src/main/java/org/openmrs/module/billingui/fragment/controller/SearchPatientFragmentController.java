95b76413f24ef373543bd892ef4d2607fe238b78
ngarivictor <ngarivictor43@gmail.com>
2016-02-10 11:18:03 +0300

    fixed out of memory error and recognise hyphen during search

--- omod/src/main/java/org/openmrs/module/billingui/fragment/controller/SearchPatientFragmentController.java
+++ omod/src/main/java/org/openmrs/module/billingui/fragment/controller/SearchPatientFragmentController.java
@@ -1,20 +1,21 @@
 package org.openmrs.module.billingui.fragment.controller;
 
-import org.hibernate.validator.constraints.SafeHtml;
+
+import org.apache.commons.lang.StringUtils;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.billingui.PatientWrapper;
 import org.openmrs.module.hospitalcore.HospitalCoreService;
+import org.openmrs.module.hospitalcore.matcher.*;
 import org.openmrs.module.hospitalcore.util.HospitalCoreConstants;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.regex.Matcher;
 
 /**
  * Created by USER on 2/9/2016.
@@ -27,7 +28,7 @@ public class SearchPatientFragmentController {
     public void controller(){}
 
     public List<SimpleObject> searchSystemPatient(
-            @RequestParam(value = "searchKey", required = false) String phrase,
+            @RequestParam(value = "phrase", required = false) String phrase,
             @RequestParam(value = "currentPage", required = false) Integer currentPage,
             @RequestParam(value = "pageSize", required = false) Integer pageSize,
             UiUtils uiUtils,
@@ -35,9 +36,9 @@ public class SearchPatientFragmentController {
         String prefix = Context.getAdministrationService().getGlobalProperty(
                 HospitalCoreConstants.PROPERTY_IDENTIFIER_PREFIX);
 //        model.addAttribute("prefix", prefix);
-        if (phrase.contains("-") && !phrase.contains(prefix)) {
+       /* if (phrase.contains("-") && !phrase.contains(prefix)) {
             phrase = prefix + phrase;
-        }
+        }*/
 
         String gender = request.getParameter("gender");
         if (gender.equalsIgnoreCase("any"))
@@ -50,18 +51,76 @@ public class SearchPatientFragmentController {
 
         HospitalCoreService hcs = (HospitalCoreService) Context
                 .getService(HospitalCoreService.class);
-        List<Patient> patients = hcs.searchPatient( phrase, gender,  age, ageRange,  date, dateRange, relativeName);
+        List<Patient> patients = hcs.searchPatient(phrase, gender, age,
+                ageRange, date, dateRange, relativeName);
         List<PatientWrapper> wrapperList = patientsWithLastVisit(patients);
-        return SimpleObject.fromCollection(wrapperList, uiUtils, "wrapperIdentifier","names", "age", "gender","lastVisitTime");
+
+        return SimpleObject.fromCollection(wrapperList, uiUtils, "patientId", "wrapperIdentifier", "names", "age", "gender","formartedVisitDate");
     }
-    private Integer getInt(String value) {
-        try {
-            Integer number = Integer.parseInt(value);
-            return number;
-        } catch (Exception e) {
-            return 0;
+
+    // Filter patient list using advance search criteria
+    private List<Patient> filterPatients(HttpServletRequest request,
+                                         List<Patient> patients) throws NumberFormatException,
+            ParseException {
+
+        List<Patient> filteredPatients = patients;
+
+        // filter using gender
+        String genderCriterion = request.getParameter("gender");
+        if (!StringUtils.isBlank(genderCriterion)) {
+            filteredPatients = select(filteredPatients, new GenderMatcher(
+                    new String(genderCriterion)));
         }
+
+        // filter using age criteria
+        String ageCriterion = request.getParameter("age");
+        if (!StringUtils.isBlank(ageCriterion)) {
+            String ageRange = request.getParameter("ageRange");
+            try {
+                filteredPatients = select(filteredPatients, new AgeMatcher(
+                        new Integer(ageCriterion), new Integer(ageRange)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new NumberFormatException("advancesearch.error.age");
+            }
+        }
+
+        // filter using relative name
+        String relativeNameCriterion = request.getParameter("relativeName");
+        if (!StringUtils.isBlank(relativeNameCriterion)) {
+            filteredPatients = select(filteredPatients,
+                    new RelativeNameMatcher(relativeNameCriterion));
+        }
+
+        // filter using date of visit
+        String dateCriterion = request.getParameter("date");
+        if (!StringUtils.isBlank(dateCriterion)) {
+            try {
+                String dateRange = request.getParameter("dateRange");
+                filteredPatients = select(filteredPatients, new DateMatcher(
+                        dateCriterion, new Integer(dateRange)));
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new NumberFormatException("advancesearch.error.date");
+            }
+        }
+
+        return filteredPatients;
     }
+
+    // paging
+    private List<Patient> pagePatient(List<Patient> patients, int currentPage,
+                                      int pageSize) {
+        int firstIndex = pageSize * currentPage;
+        List<Patient> page = new ArrayList<Patient>();
+        for (int i = firstIndex; i < (currentPage + 1) * pageSize; i++) {
+            if (i < patients.size()) {
+                page.add(patients.get(i));
+            }
+        }
+        return page;
+    }
+
     private List<PatientWrapper> patientsWithLastVisit(List<Patient> patients){
         HospitalCoreService hcs = Context.getService(HospitalCoreService.class);
         List<PatientWrapper> wrappers = new ArrayList<PatientWrapper>();
@@ -70,5 +129,27 @@ public class SearchPatientFragmentController {
         }
         return  wrappers;
     }
+
+
+    private List<Patient> select(List<Patient> patients, Matcher matcher) {
+        List<Patient> result = new ArrayList<Patient>();
+        for (Patient patient : patients) {
+            if (matcher.matches(patient)) {
+                result.add(patient);
+            }
+        }
+        return result;
+    }
+
+    private Integer getInt(String value) {
+        try {
+            Integer number = Integer.parseInt(value);
+            return number;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+
 }
 
